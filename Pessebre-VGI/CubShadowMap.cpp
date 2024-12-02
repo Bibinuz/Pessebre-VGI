779b16fd8674ddc0596cc5ed59f9467104bbef01
Biel Alavedra <biel.alavedra@gmail.com>
2024-12-02 12:30:00 +0100

    inici cubshadowmap

---
+++ Pessebre-VGI/CubShadowMap.cpp
@@ -0,0 +1,1 @@
+#include "CubShadowMap.h"
---
+++ Pessebre-VGI/CubShadowMap.h
@@ -0,0 +1,10 @@
+#ifndef CUBSHADOWMAP_CLASS_H
+#define CUBSHADOWMAP_CLASS_H
+
+#pragma once
+#include "Shadowmap.h"
+class CubShadowMap : public Shadowmap
+{
+};
+
+#endif
